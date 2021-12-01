12ca9f91831576457dbcc03c2cde5a801d95ea50
Elke Heymans <el.heymans@gmail.com>
2021-12-01 08:34:21 +0100

    Solved day01

--- src/day01.rs
+++ src/day01.rs
@@ -1,3 +1,28 @@
 pub fn part1(input: String) {
-    println!("{}", input.chars().rev().collect::<String>());
+    println!(
+        "{}",
+        input
+            .lines()
+            .map(|line| line.parse::<i32>().unwrap())
+            .collect::<Vec<i32>>()
+            .windows(2)
+            .filter(|window| &window[1] > &window[0])
+            .count()
+    );
+}
+
+pub fn part2(input: String) {
+    println!(
+        "{}",
+        input
+            .lines()
+            .map(|line| line.parse::<i32>().unwrap())
+            .collect::<Vec<i32>>()
+            .windows(3)
+            .map(|window| &window[0] + &window[1] + &window[2])
+            .collect::<Vec<i32>>()
+            .windows(2)
+            .filter(|window| &window[1] > &window[0])
+            .count()
+    );
 }
