e34db64c02430f068a1ace6f446c6ceda7231696
Elke Heymans <el.heymans@gmail.com>
2021-12-02 19:48:45 +0100

    Formatting day 2

--- src/day02.rs
+++ src/day02.rs
@@ -19,38 +19,32 @@ fn parse_line(line: &str) -> Direction {
 
 pub fn part1(input: String) {
     let parsed_lines = input.lines().map(parse_line);
-	let mut depth = 0;
-	let mut horizontal = 0;
-	for line in parsed_lines {
-		match line {
+    let mut depth = 0;
+    let mut horizontal = 0;
+    for line in parsed_lines {
+        match line {
             Direction::Forward(distance) => horizontal += distance,
             Direction::Down(distance) => depth += distance,
             Direction::Up(distance) => depth -= distance,
         }
-	}
-    println!(
-        "{}",
-        depth * horizontal
-    );
+    }
+    println!("{}", depth * horizontal);
 }
 
 pub fn part2(input: String) {
     let parsed_lines = input.lines().map(parse_line);
-	let mut depth = 0;
-	let mut horizontal = 0;
-	let mut aim = 0;
-	for line in parsed_lines {
-		match line {
+    let mut depth = 0;
+    let mut horizontal = 0;
+    let mut aim = 0;
+    for line in parsed_lines {
+        match line {
             Direction::Forward(distance) => {
-				horizontal += distance;
-				depth += aim * distance;
-			},
+                horizontal += distance;
+                depth += aim * distance;
+            }
             Direction::Down(distance) => aim += distance,
             Direction::Up(distance) => aim -= distance,
         }
-	}
-    println!(
-        "{}",
-        depth * horizontal
-    );
+    }
+    println!("{}", depth * horizontal);
 }
