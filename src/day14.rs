8728da9399d344d7ce806ba6b90059fa9709b5a7
Elke Heymans <el.heymans@gmail.com>
2021-12-14 22:53:30 +0100

    Clenaed up day 14

--- src/day14.rs
+++ src/day14.rs
@@ -1,12 +1,10 @@
-use std::{
-    cmp::max,
-    collections::{HashMap, HashSet},
-    ops::Add,
-    vec,
-};
+use std::{collections::HashMap, ops::Add};
 
-fn parse_input(input: &String) -> (String, HashMap<String, String>) {
-    let mut rules = HashMap::new();
+type Polymer = HashMap<String, u64>;
+type Rules = HashMap<String, String>;
+
+fn parse_input(input: &String) -> (Polymer, Rules) {
+    let mut rules = Rules::new();
     let mut lines = input.lines();
     let polymer_template = lines.next().unwrap();
     lines.next();
@@ -14,80 +12,35 @@ fn parse_input(input: &String) -> (String, HashMap<String, String>) {
         let (key, value) = insertion_rule.split_once(" -> ").unwrap();
         rules.insert(key.to_string(), value.to_string());
     }
-    (polymer_template.to_string(), rules)
-}
 
-pub fn part1(input: String) {
-    fn step(polymer: String, rules: &HashMap<String, String>, steps_to_go: u8) -> String {
-        if steps_to_go == 0 {
-            polymer
-        } else {
-            let mut new_polymer = String::new().to_owned();
-            for i in 0..polymer.len() - 1 {
-                new_polymer.push_str(&polymer[i..i + 1]);
-                if let Some(insertion) = rules.get(&polymer[i..=i + 1]) {
-                    new_polymer.push_str(insertion);
-                }
-            }
-            new_polymer.push_str(&polymer[polymer.len() - 1..polymer.len()]);
-            step(new_polymer, rules, steps_to_go - 1)
-        }
-    }
-
-    let (polymer, rules) = parse_input(&input);
-    let new_polymer = step(polymer, &rules, 10);
-
-    let mut counts: HashMap<char, u32> = HashMap::new();
-    for c in new_polymer.chars() {
-        *counts.entry(c).or_insert(0) += 1;
-    }
-
-    let mut most_common_element = counts.iter().nth(0).unwrap();
-    let mut least_common_element = counts.iter().nth(0).unwrap();
-
-    for (element, occurences) in &counts {
-        if occurences > most_common_element.1 {
-            most_common_element = (element, occurences);
-        }
-        if occurences < least_common_element.1 {
-            least_common_element = (element, occurences);
-        }
-    }
-
-    println!("{:?}", most_common_element.1 - least_common_element.1); // 3213
-}
-
-pub fn part2(input: String) {
-    fn step(
-        polymer: HashMap<String, u64>,
-        rules: &HashMap<String, String>,
-        steps_to_go: u8,
-    ) -> HashMap<String, u64> {
-        // println!("{:?}", polymer);
-        if steps_to_go == 0 {
-            polymer
-        } else {
-            let mut new_polymer = HashMap::new();
-            for (s, n) in polymer.iter() {
-                if let Some(insertion) = rules.get(s) {
-                    let new_s_1 = String::from(&s[0..1]).add(insertion);
-                    let new_s_2 = String::from(insertion).add(&s[1..2]);
-                    *new_polymer.entry(new_s_1).or_insert(0) += n;
-                    *new_polymer.entry(new_s_2).or_insert(0) += n;
-                }
-            }
-            step(new_polymer, rules, steps_to_go - 1)
-        }
-    }
-    let (polymer, rules) = parse_input(&input);
-    let mut parsed_polymer: HashMap<String, u64> = HashMap::new();
+    let mut parsed_polymer = Polymer::new();
+    let polymer = polymer_template.to_string();
     for i in 0..polymer.len() - 1 {
         *parsed_polymer
             .entry(String::from(&polymer[i..=i + 1]))
             .or_insert(0) += 1;
     }
-    let polymer_result = step(parsed_polymer, &rules, 40);
+    (parsed_polymer, rules)
+}
 
+fn step(polymer: Polymer, rules: &Rules, steps: u8) -> Polymer {
+    if steps == 0 {
+        polymer
+    } else {
+        let mut new_polymer = Polymer::new();
+        for (s, n) in polymer.iter() {
+            if let Some(insertion) = rules.get(s) {
+                let new_s_1 = String::from(&s[0..1]).add(insertion);
+                let new_s_2 = String::from(insertion).add(&s[1..2]);
+                *new_polymer.entry(new_s_1).or_insert(0) += n;
+                *new_polymer.entry(new_s_2).or_insert(0) += n;
+            }
+        }
+        step(new_polymer, rules, steps - 1)
+    }
+}
+
+fn calculate_result(polymer_result: Polymer) -> u64 {
     let mut parsed_results = HashMap::new();
     for (element, occurences) in &polymer_result {
         let first_char = element.chars().nth(0).unwrap();
@@ -108,5 +61,19 @@ pub fn part2(input: String) {
             least_common_element = (element, occurences);
         }
     }
-    println!("{:?}", most_common_element.1 - least_common_element.1 + 1); // 3213
+    most_common_element.1 - least_common_element.1 + 1
+}
+
+pub fn part1(input: String) {
+    let (polymer, rules) = parse_input(&input);
+    let polymer_result = step(polymer, &rules, 10);
+
+    println!("{:?}", calculate_result(polymer_result)); // 3213
+}
+
+pub fn part2(input: String) {
+    let (polymer, rules) = parse_input(&input);
+    let polymer_result = step(polymer, &rules, 40);
+
+    println!("{:?}", calculate_result(polymer_result)); // 3711743744429
 }
